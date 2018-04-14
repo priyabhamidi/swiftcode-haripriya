f3bef07a3d80618f2392f5bd565e003f16dc004e
priyabhamidi <bhamidi.haripriya@gmail.com>
2018-04-14 12:12:18 +0000

    chat

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,8 +1,22 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
-
-
-
-
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+	},
+        {
+            'sender': 'BOT',
+            'text': 'hey..!!'
+	},
+        {
+            'sender': 'USER',
+            'text': 'how r u'
+	},
+        {
+            'sender': 'BOT',
+            'text': 'fine'
+	}
+	];
 });
