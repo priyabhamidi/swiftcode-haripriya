65c5e8484eb0cc5abf20b464f1cdedbefc8f9eb9
priyabhamidi <bhamidi.haripriya@gmail.com>
2018-04-15 06:02:02 +0000

    messageactor

--- app/actors/MessageActor.java
+++ app/actors/MessageActor.java
@@ -6,15 +6,47 @@ import akka.actor.UntypedActor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import data.FeedResponse;
 import data.Message;
+import data.NewsAgentResponse;
 import services.FeedService;
 import services.NewsAgentService;
 
 import java.util.UUID;
 
 public class MessageActor extends UntypedActor {
+    //props
+    public static Props props(ActorRef out) {
+        return Props.create(MessageActor.class, out);
+    }
+
+    private final ActorRef out;
+
+    public MessageActor(ActorRef out) {
+        this.out = out;
+    }
+
+    private FeedService feedService = new FeedService();
+    private NewsAgentService newsAgentService = new NewsAgentService();
+    // public FeedResponse feedResponse=new FeedResponse();
+    private NewsAgentResponse newsAgentResponse=new NewsAgentResponse();
 
     @Override
     public void onReceive(Object message) throws Throwable {
+        ObjectMapper mapper = new ObjectMapper();
+        Message messageObject = new Message();
+        if (message instanceof String) {
+            //Message messageObject = new Message();
+            messageObject.text=(String)message;
+            messageObject.sender=Message.Sender.USER;
+            out.tell(mapper.writeValueAsString(messageObject), self());
+            //newsAgentService.getNewsAgentResponse(messageObject.text,UUID.randomUUID());
+            String query=newsAgentService.getNewsAgentResponse("Find " + message,UUID.randomUUID()).query;
+            FeedResponse feedResponse=feedService.getFeedByQuery(query);
+            messageObject.text = (feedResponse.title == null) ? "No results found" : "Showing results for: " + query;
+            messageObject.feedResponse=feedResponse;
+            messageObject.sender=Message.Sender.BOT;
+            out.tell(mapper.writeValueAsString(messageObject), self());
+        }
+
 
     }
 }
