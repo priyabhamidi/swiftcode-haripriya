17619b381720cd53ca28babf7831a0d83b50dbbe
priyabhamidi <bhamidi.haripriya@gmail.com>
2018-04-15 10:51:28 +0000

    msgnews

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -7,22 +7,22 @@ app.config(function ($mdThemingProvider) {
 });
 
 app.controller('chatController', function ($scope) {
-    $scope.messages = [
-        {
-            'sender': 'USER',
-            'text': 'Hello'
-	},
-        {
-            'sender': 'BOT',
-            'text': 'hey..!!'
-	},
-        {
-            'sender': 'USER',
-            'text': 'how r u'
-	},
-        {
-            'sender': 'BOT',
-            'text': 'fine'
-	}
-	];
+    $scope.messages = [];
+
+    var exampleSocket = new WebSocket('ws://localhost:9000/chatSocket');
+
+    exampleSocket.onmessage = function (event) {
+        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date()
+            .toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply();
+        console.log(jsonData);
+
+    };
+
+    $scope.sendMessage = function () {
+        exampleSocket.send($scope.userMessage);
+        $scope.userMessage = '';
+    };
 });
